f744a81af8641181f2def3863f99dce34c034832
Akihiro Uehara <u-akihiro@reinforce-lab.com>
2010-08-20 17:38:18 +0900

    Audio route setting methods are added in AudioUnitUtils.cs

--- MonoTouch_AudioUnit/AudioUnitUtils.cs
+++ MonoTouch_AudioUnit/AudioUnitUtils.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace MonoTouch.AudioToolbox
 {
@@ -28,5 +28,53 @@ namespace MonoTouch.AudioToolbox
             };
             return audioFormat;
         }
+
+        public static void SetOverrideCategoryDefaultToSpeaker(bool isSpeaker)
+        {
+            int val = isSpeaker ? 1 : 0;
+            int err = AudioSessionSetProperty(
+                0x6373706b, //'cspk'
+                (UInt32)sizeof(UInt32),
+                ref val);
+            if (err != 0)
+                throw new ArgumentException();            
+        }
+             
+        /*
+        public static void SetOverrideAudioRoute(OverrideAudioRouteType route)
+        {
+            int err = AudioSessionSetProperty(
+                AudioSessionProperty.OverrideAudioRoute,
+                (UInt32)sizeof(UInt32),
+                ref route);
+            if (err != 0)
+                throw new ArgumentException();
+        }        
+        */
+
+        /*
+Int32 doChangeDefaultRoute = 1;
+         * AudioSessionSetProperty 
+         * (kAudioSessionProperty_OverrideCategoryDefaultToSpeaker,
+         * sizeof (doChangeDefaultRoute),
+         * &doChangeDefaultRou         */
+        #region Interop
+        public enum OverrideAudioRouteType
+        {
+            None = 0,
+            Speaker = 0x73706d72 // 'spkr'
+        }
+        [DllImport(MonoTouch.Constants.AudioToolboxLibrary, EntryPoint = "AudioSessionSetProperty")]
+        static extern int AudioSessionSetProperty(
+            AudioSessionProperty inID,
+            UInt32 inDataSize,
+            ref OverrideAudioRouteType inData);
+        
+        [DllImport(MonoTouch.Constants.AudioToolboxLibrary, EntryPoint = "AudioSessionSetProperty")]
+        static extern int AudioSessionSetProperty(
+            UInt32 inID,
+            UInt32 inDataSize,
+            ref int inData);
+        #endregion
     }
 }
